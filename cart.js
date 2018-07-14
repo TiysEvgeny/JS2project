e26837b7950adf5f353d9dadca89504c44220b10
TiysEvgeny <tiys@ngs.ru>
2018-07-15 00:02:06 +0700

    Сделал добавление в корзину, при условии, что такой товар уже есть в корзине

--- cart.js
+++ cart.js
@@ -2,20 +2,25 @@
 
 function buildCart() {
     $('.cart-and-account__cart-content').empty();
-    $('.cart-and-account__cart-content').append($('<div/>', {class: 'arrow-up'}));
+    $('.cart-and-account__cart-content').append($('<div/>', {
+        class: 'arrow-up'
+    }));
     $.get('http://localhost:3000/cart', {}, function (items) {
         var $ul = $('<ul/>');
-        items.content.forEach (function (item) {
+        var totalPrice = 0;
+        items.forEach(function (item) {
             var $li = $('<li/>');
-            var $itemName=$('<div/>', {
+            var $itemName = $('<div/>', {
                 class: 'cart-and-account__itemName',
                 itemId: item.id,
+                quantity: item.quantity,
                 text: item.name
             });
-            var $itemQuantity=$('<div/>', {
+            var $itemQuantity = $('<div/>', {
                 class: 'cart-and-account__itemQuantity',
                 text: item.quantity + ' X $' + item.price
             });
+            totalPrice += item.quantity * item.price
             $ul.append($li);
             $li.append($itemName);
             $li.append($itemQuantity);
@@ -27,22 +32,54 @@ function buildCart() {
             text: 'TOTAL'
         });
         var $totalPrice = $('<div/>', {
-            text: '$' + items.total
+            text: '$' + totalPrice
         });
         $('.cart-and-account__cart-content').append($ul);
         $total.append($totalLabel);
-        $total.append($totalPrice);    
+        $total.append($totalPrice);
         $('.cart-and-account__cart-content').append($total);
-        
-    },'json');
+
+    }, 'json');
 }
 
-(function($){
-    $(function(){
+(function ($) {
+    $(function () {
         buildCart();
-      $('.items__item').on('click', '.items__add-to-cart-link', function(event){
+        $('.items__item').on('click', '.items__add-to-cart-link', function (event) {
+            var $goodId = $(this).parent().attr('itemId');
+            var $goodName = $(this).parent().attr('name');
+            var $goodPrice = $(this).parent().attr('price');
+            var $cartItem = $('.cart-and-account__itemName[itemId="' + $goodId + '"]')
+            var quantity = $.get('http://localhost:3000/goods/' + $goodId, {}, function (item) {
+                return item.quantity;
+            }, 'json');
+            if (quantity < +$cartItem.attr('quantity') + 1) {
+                alert("На складе недостаточно товара!");
+                return;
+            } else {
+                if ($cartItem.length) {
+                    var good = {
+                        id: $goodId,
+                        name: $goodName,
+                        price: $goodPrice,
+                        quantity: +$cartItem.attr('quantity') + 1
+                    };
+                    $.ajax({
+                        url: 'http://localhost:3000/cart/' + $goodId,
+                        type: 'PUT',
+                        data: good,
+                        success: function () {
+                            buildCart();
+                            return {
+                                result: 1
+                            };
+                        }
+                    });
+                } else{
 
-          event.preventDefault();
-      });
-    }); 
- })(jQuery);
+                }
+            }
+            event.preventDefault();
+        });
+    });
+})(jQuery);
